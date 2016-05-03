2c99ee82efb3a8fcaa8d2e227b1700383d0d6f25
Christian <Christian@Christian-PC>
2016-05-03 09:09:20 -0500

    clase1

--- ChicaizaRobinson/src/chicaizarobinson/HOLAcb.java
+++ ChicaizaRobinson/src/chicaizarobinson/HOLAcb.java
@@ -10,5 +10,5 @@ package chicaizarobinson;
  * @author Christian
  */
 public class HOLAcb {
-    
+   
 }
--- ChicaizaRobinson/src/chicaizarobinson/SumaChicaizaRobinson.java
+++ ChicaizaRobinson/src/chicaizarobinson/SumaChicaizaRobinson.java
@@ -15,7 +15,7 @@ public class SumaChicaizaRobinson {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola compañeros");
+        System.out.println("hola compañeros>>>>>>>>>>>>>> hola robin soy chis");
     }
     
 }
